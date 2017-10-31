fd46aa63d06a36a01eae839e67dc5ccd300e19ff
LL <nzlili2015@gmail.com>
2017-10-31 11:22:48 +0800

    Create the project.

---
+++ ExpressionTreeHelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTreeHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockExpression blockExpr = Expression.Block(
+                Expression.Call(null, typeof(Console).GetMethod("Write", new Type[] { typeof(String) }), Expression.Constant("Hello ")),
+                Expression.Call(null, typeof(Console).GetMethod("WriteLine", new Type[] { typeof(String) }), Expression.Constant("World!")),
+                Expression.Call(null, typeof(Console).GetMethod("ReadLine")));
+            Expression.Lambda<Action>(blockExpr).Compile()();
+        }
+    }
+}
